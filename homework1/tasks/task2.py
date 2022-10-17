12dd3afabe332b94dd2b86ae4311c430b74a5f8a
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-10-17 21:44:10 +0300

    reformat code for task2

--- homework1/tasks/task2.py
+++ homework1/tasks/task2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
-from homework1.abstract_task import AbstractTask
+from homework1.tasks.abstract_task import AbstractTask
 from homework1.defaults import RESULT_ROOT
 
 
@@ -13,26 +13,26 @@ class Task2(AbstractTask):
     def __init__(self, graph: nx.Graph):
         super().__init__(graph=graph)
 
-        self.degree_centrality = None
-        self.closeness_centrality = None
-        self.betweenness_centrality = None
-        self.eigenvector_centrality = None
-        self.cosine_similarity = None
+        self._degree_centrality = None
+        self._closeness_centrality = None
+        self._betweenness_centrality = None
+        self._eigenvector_centrality = None
+        self._cosine_similarity = None
 
     def run(self):
-        self.degree_centrality = nx.centrality.degree_centrality(self.G)
-        self.closeness_centrality = nx.centrality.closeness_centrality(self.G)
-        self.betweenness_centrality = nx.centrality.betweenness_centrality(self.G)
-        self.eigenvector_centrality = nx.centrality.eigenvector_centrality(self.G)
+        self._degree_centrality = nx.centrality.degree_centrality(self.G)
+        self._closeness_centrality = nx.centrality.closeness_centrality(self.G)
+        self._betweenness_centrality = nx.centrality.betweenness_centrality(self.G)
+        self._eigenvector_centrality = nx.centrality.eigenvector_centrality(self.G)
 
-        self.get_top_10(self.degree_centrality, 'Degree Centrality')
-        self.get_top_10(self.closeness_centrality, 'Closeness Centrality')
-        self.get_top_10(self.betweenness_centrality, 'Betweenness Centrality')
-        self.get_top_10(self.eigenvector_centrality, 'Eigenvector Centrality')
+        self._get_top_10(self._degree_centrality, 'Degree Centrality')
+        self._get_top_10(self._closeness_centrality, 'Closeness Centrality')
+        self._get_top_10(self._betweenness_centrality, 'Betweenness Centrality')
+        self._get_top_10(self._eigenvector_centrality, 'Eigenvector Centrality')
 
         adj_matrix = nx.to_scipy_sparse_array(self.G)
-        self.cosine_similarity = cosine_similarity(adj_matrix)
-        cs = self.cosine_similarity.copy()
+        self._cosine_similarity = cosine_similarity(adj_matrix)
+        cs = self._cosine_similarity.copy()
         n = len(cs)
         for i in range(n):
             for j in range(n):
@@ -55,16 +55,16 @@ class Task2(AbstractTask):
             x_name = list(nx.nodes(self.G))[x]
             y_name = list(nx.nodes(self.G))[y]
 
-            print(f'{x_name:15} + {y_name:20}: {self.cosine_similarity[x, y]:.3f}')
+            print(f'{x_name:15} + {y_name:20}: {self._cosine_similarity[x, y]:.3f}')
 
-        plt.matshow(self.cosine_similarity)
+        plt.matshow(self._cosine_similarity)
         title = 'Cosine Similarity'
         plt.title(title)
         plt.savefig(RESULT_ROOT / f'{title}.png')
         plt.close('all')
 
     @staticmethod
-    def get_top_10(metric: dict, tag: str):
+    def _get_top_10(metric: dict, tag: str):
         lst = sorted(metric.items(), key=lambda x: x[1], reverse=True)
         print(f'{tag}:')
         for name, value in lst[:10]:
