5e3da2c0c2fe8feb5d9c950ca626fe1a17dd8eff
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-10-17 22:51:40 +0300

    added task3; code optimization

--- homework1/run.py
+++ homework1/run.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import logging
 
 from defaults import GRAPH_ROOT
 from tasks.task1 import Task1
@@ -6,13 +7,15 @@ from tasks.task2 import Task2
 from tasks.task3 import Task3
 
 # TODO: add logging
+# TODO: translate .gml to .yaml and work with DictConfig in future
 
 
 def main():
     g = nx.read_gml(GRAPH_ROOT / 'netscience.gml')
 
-    tasks = [Task1, Task2, Task3]
-    for Task in tasks:
+    for Task in [Task1,
+                 Task2,
+                 Task3]:
         print(f'Run {Task.prefix}')
         task = Task(g)
         task.run()
