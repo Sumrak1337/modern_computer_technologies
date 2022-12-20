b7ae63a0f91ce93394dbd069e7bbfd75dda5ae1a
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-12-20 17:43:33 +0300

    some hw1's files changes

--- homework1/task_defaults.py
+++ homework1/task_defaults.py
@@ -1,5 +1,8 @@
+import os
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).absolute().parent
 RESULT_ROOT = PROJECT_ROOT / 'results'
 DATA_ROOT = PROJECT_ROOT / 'data'
+
+os.makedirs(RESULT_ROOT, exist_ok=True)
